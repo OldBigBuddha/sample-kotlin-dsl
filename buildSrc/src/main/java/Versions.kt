00a0618f1b7f0f6e5d261059eb722f043f296928
OldBigBuddha <development@oldbigbuddha.net>
2019-02-23 20:13:30 +0200

    [add] Versions.kt

---
+++ buildSrc/src/main/java/Versions.kt
@@ -0,0 +1,11 @@
+object Versions {
+    const val kotlin = "1.3.21"
+    const val gradlePlugin = "3.3.1"
+
+    const val compileSdk = 28
+    const val targetSdk = 28
+    const val minSdk = 21
+
+    const val code = 1
+    const val name = "1.0"
+}
